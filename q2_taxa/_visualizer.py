d377b9de10f9e76a285ef3e4f07e0c7275eb30f1
Keegan Evans <keegan.evans@gmail.com>
2021-03-12 14:00:58 -0700

    imp: remove requirement for metadata input

--- q2_taxa/_visualizer.py
+++ q2_taxa/_visualizer.py
@@ -16,54 +16,91 @@ import q2templates
 
 from qiime2 import Metadata
 
-from ._util import _extract_to_level
+from  ._util import _extract_to_level
 
 
 TEMPLATES = pkg_resources.resource_filename('q2_taxa', 'assets')
 
 
 def barplot(output_dir: str, table: pd.DataFrame, taxonomy: pd.Series,
-            metadata: Metadata) -> None:
-    ids_not_in_metadata = set(table.index) - set(metadata.ids)
-    if ids_not_in_metadata:
-        raise ValueError('Sample IDs found in the table are missing in the '
-                         f'metadata: {ids_not_in_metadata!r}.')
+            metadata: Metadata = None) -> None:
+    if metadata == None:
+        print("there is no metadata")
+        set(table.index)
 
-    metadata = metadata.to_dataframe()
-    jsonp_files, csv_files = [], []
-    collapsed_tables = _extract_to_level(taxonomy, table)
+        jsonp_files, csv_files = [], []
+        collapsed_tables = _extract_to_level(taxonomy, table)
+        
+        for level, df in enumerate(collapsed_tables, 1):
+             print(level)
+             # Stash column labels before manipulating dataframe
+             taxa_cols = df.columns.values.tolist()
+             # Join collapsed table with metadata
+             df = df.reset_index(drop=False)  # Move index into columns
+             # Our JS sort works best with empty strings vs nulls
+             df = df.fillna('')
+             all_cols = df.columns.values.tolist()
+ 
+             jsonp_file = 'level-%d.jsonp' % level
+             csv_file = 'level-%d.csv' % level
+ 
+             jsonp_files.append(jsonp_file)
+             csv_files.append(csv_file)
+ 
+             df.to_csv(os.path.join(output_dir, csv_file), index=False)
+ 
+             with open(os.path.join(output_dir, jsonp_file), 'w') as fh:
+                 fh.write('load_data(%d,' % level)
+                 json.dump(taxa_cols, fh)
+                 fh.write(',')
+                 json.dump(all_cols, fh)
+                 fh.write(',')
+                 df.to_json(fh, orient='records')
+                 fh.write(');')
+ 
+ 
+    else:      
+        ids_not_in_metadata = set(table.index) - set(metadata.ids)
+        if ids_not_in_metadata:
+            raise ValueError('Sample IDs found in the table are missing in the '
+                             f'metadata: {ids_not_in_metadata!r}.')
 
-    for level, df in enumerate(collapsed_tables, 1):
-        # Stash column labels before manipulating dataframe
-        taxa_cols = df.columns.values.tolist()
-        # Join collapsed table with metadata
-        df = df.join(metadata, how='left')
-        df = df.reset_index(drop=False)  # Move index into columns
-        # Our JS sort works best with empty strings vs nulls
-        df = df.fillna('')
-        all_cols = df.columns.values.tolist()
+        metadata = metadata.to_dataframe()
+        jsonp_files, csv_files = [], []
+        collapsed_tables = _extract_to_level(taxonomy, table)
 
-        jsonp_file = 'level-%d.jsonp' % level
-        csv_file = 'level-%d.csv' % level
+        for level, df in enumerate(collapsed_tables, 1):
+            # Stash column labels before manipulating dataframe
+            taxa_cols = df.columns.values.tolist()
+            # Join collapsed table with metadata
+            df = df.join(metadata, how='left')
+            df = df.reset_index(drop=False)  # Move index into columns
+            # Our JS sort works best with empty strings vs nulls
+            df = df.fillna('')
+            all_cols = df.columns.values.tolist()
 
-        jsonp_files.append(jsonp_file)
-        csv_files.append(csv_file)
+            jsonp_file = 'level-%d.jsonp' % level
+            csv_file = 'level-%d.csv' % level
 
-        df.to_csv(os.path.join(output_dir, csv_file), index=False)
+            jsonp_files.append(jsonp_file)
+            csv_files.append(csv_file)
 
-        with open(os.path.join(output_dir, jsonp_file), 'w') as fh:
-            fh.write('load_data(%d,' % level)
-            json.dump(taxa_cols, fh)
-            fh.write(',')
-            json.dump(all_cols, fh)
-            fh.write(',')
-            df.to_json(fh, orient='records')
-            fh.write(');')
+            df.to_csv(os.path.join(output_dir, csv_file), index=False)
+
+            with open(os.path.join(output_dir, jsonp_file), 'w') as fh:
+                fh.write('load_data(%d,' % level)
+                json.dump(taxa_cols, fh)
+                fh.write(',')
+                json.dump(all_cols, fh)
+                fh.write(',')
+                df.to_json(fh, orient='records')
+                fh.write(');')
 
     # Now that the tables have been collapsed, write out the index template
     index = os.path.join(TEMPLATES, 'barplot', 'index.html')
     q2templates.render(index, output_dir, context={'jsonp_files': jsonp_files})
+    print("template render")
 
     # Copy assets for rendering figure
     shutil.copytree(os.path.join(TEMPLATES, 'barplot', 'dist'),
-                    os.path.join(output_dir, 'dist'))
+            os.path.join(output_dir, 'dist'))
